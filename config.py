dce826b759bb4f06dff0ee01f5917e59ab374094
Komal <komalnaseem147@gmail.com>
2025-08-26 22:16:52 +0500

    add config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+
+# Your test Microsoft account credentials (replace with yours; remove after testing for security)
+EMAIL = ""
+PASSWORD = ""
+
+# Dummy queries to send
+DUMMY_QUERIES = [
+    "What is a json file?"
+]
